38b85551966df3085ed98a956e9c25b58964ea5b
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-03-30 15:01:44 +0530

    Update Tree,Indirect,Nested recursion.cpp

--- Recursion/Tree,Indirect,Nested recursion.cpp
+++ Recursion/Tree,Indirect,Nested recursion.cpp
@@ -8,8 +8,29 @@ if(n>0){
      fun1(n-1); //it will be executed only after the 1st get's terminated fully
 }
 }
+
+void funB(int n); //need to initialize funB globally it's called in funA which is before funB
+void funA(int n)
+{
+ if(n>0)
+ {
+ printf("%d,",n);
+ funB(n-1);
+ }
+}
+void funB(int n)
+{
+ if(n>1)
+ {
+ printf("%d,",n);
+ funA(n/2);
+ }
+}
+
 int main()
 {
     cout <<"Tree recursion";
     tree(3); //3,2,1,1,2,1,1
+   cout <<"Indirect recursion";
+   funA(20); //20,19,9,8,4,3,1
 }
