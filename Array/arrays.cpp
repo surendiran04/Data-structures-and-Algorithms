52043538b51066ef26b71588a60413fd3879bd62
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-04-05 16:07:30 +0530

    Create arrays.cpp

---
+++ Array/arrays.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ //collections of items of same data type stored in contigous memory locations
+int main()
+{
+    int A[]={1,2,3,4,5};  //A[5]={1,2,3,4,5} ,A[5]={0} ,A[5]={1,2}
+    for(int i=0;i<sizeof(A)/sizeof(A[0]);i++){
+        printf("%d\n",*(A+i)); // printf("%d\n",i[A]);  printf("%d\n",A[i]);
+    }
+    return 0;
+}
