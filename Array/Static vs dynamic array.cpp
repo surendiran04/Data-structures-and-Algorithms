7043f0a02c1f90c3eb44a8dafe70663d5d7628ee
surendiran04 <137646072+surendiran04@users.noreply.github.com>
2024-04-05 16:35:46 +0530

    Update Static vs dynamic array.cpp

--- Array/Static vs dynamic array.cpp
+++ Array/Static vs dynamic array.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include<stdlib.h>
 using namespace std;
 
 int main()
 { 
-    //A[5] - static stack memory
-    int n; 
-    cin>>n;
-    int A[n];//dynamic stack memory
+    // A[5] - static stack memory
+    // int n; 
+    // cin>>n;
+    // int A[n]; - dynamic stack memory
     
+    int A[5]={1,2,3,4,5};
+    for(int i=0;i<5;i++){
+        cout<<A[i]<<",";
+    }
+    cout<<endl;
+    int *p;
+    p=(int*)malloc(5*sizeof(int)); //head memory
+    //p=new int[5];
+    p[0]=6;
+    p[1]=7;
+    p[2]=8;
+    p[3]=9;
+    p[4]=10;
+    for(int i=0;i<5;i++){
+        cout<<p[i]<<",";
+    }
+    free(p); - deleting the heap memory after usage 
+    //delete []p;    
     return 0;
 }
