57115ee31b994970e867596a2df9482120d46150
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-11 18:20:03 +0530

    Update Array ADT.cpp

--- Array/Array ADT.cpp
+++ Array/Array ADT.cpp
@@ -175,6 +175,30 @@ int isSorted(struct Array arr) //checking an array is sorted or not
     }
     return 1;
  }
+ struct Array* Merge(struct Array *arr1,struct Array *arr2) //merging two arrays into a new array in a sorted order
+ {
+    int i,j,k;
+    i=j=k=0;
+    
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct 
+Array));
+    
+    while(i<arr1->length && j<arr2->length)
+    {
+            if(arr1->A[i]<arr2->A[j])
+                arr3->A[k++]=arr1->A[i++];
+            else
+                arr3->A[k++]=arr2->A[j++];
+    }
+    for(;i<arr1->length;i++)
+        arr3->A[k++]=arr1->A[i];
+    for(;j<arr2->length;j++)
+        arr3->A[k++]=arr2->A[j];
+    arr3->length=arr1->length+arr2->length;
+    arr3->size=10;
+    
+    return arr3;
+ }
  int main()
  {
      struct Array arr1;
@@ -189,6 +213,9 @@ int isSorted(struct Array arr) //checking an array is sorted or not
     Insert(&arr1,0,1);
     Append(&arr1,7);
     printf("%d",Delete(&arr1,0));
+    // struct Array arr2={{2,3,16,18,28},10,5};
+    // struct Array *arr3; 
+    // arr3=Merge(&arr1,&arr2);
     Display(arr1);
     free(arr1.A);
     return 0;
