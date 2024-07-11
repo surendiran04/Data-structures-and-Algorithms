8e814a107778fba4cf1bdf8bf4fcfc3fc1762651
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-11 23:51:55 +0530

    Update Array ADT.cpp

--- Array/Array ADT.cpp
+++ Array/Array ADT.cpp
@@ -180,8 +180,7 @@ int isSorted(struct Array arr) //checking an array is sorted or not
     int i,j,k;
     i=j=k=0;
     
-    struct Array *arr3=(struct Array *)malloc(sizeof(struct 
-Array));
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct Array));
     
     while(i<arr1->length && j<arr2->length)
     {
@@ -198,6 +197,82 @@ Array));
     arr3->size=10;
     
     return arr3;
+ }
+struct Array* Union(struct Array *arr1,struct Array *arr2)  //similiar to merge fn
+ {
+    int i,j,k;
+    i=j=k=0;
+    
+struct Array *arr3=(struct Array *)malloc(sizeof(struct Array));
+    
+    while(i<arr1->length && j<arr2->length)
+    {
+        if(arr1->A[i]<arr2->A[j])
+            arr3->A[k++]=arr1->A[i++];
+        else if(arr2->A[j]<arr1->A[i])
+            arr3->A[k++]=arr2->A[j++];
+        else
+        {
+            arr3->A[k++]=arr1->A[i++];
+            j++;
+        }
+    }
+    for(;i<arr1->length;i++)
+        arr3->A[k++]=arr1->A[i];
+    for(;j<arr2->length;j++)
+        arr3->A[k++]=arr2->A[j];
+    
+    arr3->length=k;
+    arr3->size=10;
+    
+    return arr3;
+}
+ struct Array* Intersection(struct Array *arr1,struct Array 
+*arr2)
+ {
+    int i,j,k;
+    i=j=k=0;
+    
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct Array));
+    
+    while(i<arr1->length && j<arr2->length)
+    {
+        if(arr1->A[i]<arr2->A[j])
+            i++;
+        else if(arr2->A[j]<arr1->A[i])
+            j++;
+        else if(arr1->A[i]==arr2->A[j])
+        {
+            arr3->A[k++]=arr1->A[i++];
+            j++;
+        }
+    }
+    arr3->length=k;
+    arr3->size=10;  
+    return arr3;
+ }
+ struct Array* Difference(struct Array *arr1,struct Array *arr2)
+ {
+    int i,j,k;
+    i=j=k=0;
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct Array)); 
+    while(i<arr1->length && j<arr2->length)
+    {
+        if(arr1->A[i]<arr2->A[j])
+            arr3->A[k++]=arr1->A[i++];
+        else if(arr2->A[j]<arr1->A[i])
+            j++;
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+ for(;i<arr1->length;i++)
+        arr3->A[k++]=arr1->A[i];
+    arr3->length=k;
+    arr3->size=10;
+ return arr3;
  }
  int main()
  {
