81bbb950eda9fbf03067de4e3fe08c5dee2945cd
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-11 12:53:34 +0530

    Update Array ADT.cpp

--- Array/Array ADT.cpp
+++ Array/Array ADT.cpp
@@ -107,6 +107,32 @@ void Insert(struct Array *arr,int index,int x)
     }
     return 0;
  }
+ void swap(int *x,int *y)
+ {
+     int temp=*x;
+     *x=*y;
+     *y=temp;
+ }
+ void Reverse(struct Array *arr)
+ {
+    int *B;
+    int i,j;
+    
+    B=(int *)malloc(arr->length*sizeof(int));
+    for(i=arr->length-1,j=0;i>=0;i--,j++)
+        B[j]=arr->A[i];
+    for(i=0;i<arr->length;i++)
+        arr->A[i]=B[i];
+    
+}
+ void Reverse2(struct Array *arr)
+ {
+    int i,j;
+    for(i=0,j=arr->length-1;i<j;i++,j--)
+    {
+        swap(&arr->A[i],&arr->A[j]);
+    }
+ }
  int main()
  {
      struct Array arr1;
