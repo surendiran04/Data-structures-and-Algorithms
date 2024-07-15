9b56c1660d5aed8d08d2d1e3c54bdbf0da5523c4
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-15 16:31:09 +0530

    Update Array exercises.cpp

--- Array/Array exercises.cpp
+++ Array/Array exercises.cpp
@@ -1,3 +1,4 @@
+//To find missing elements starting from 1 to n
 #include <iostream>
 using namespace std;
 int missingElement(int A[],int n){
@@ -12,5 +13,24 @@ int main(){
     int A[] = {1,2,3,4,5,6,8};
      int n = sizeof(A)/sizeof(A[0]);  //find array size and pass here ...Don't find size in the fn ....It treats the array as a pointer
     cout<<"Missing element is: "<<missingElement(A,n);
+    return 0;
+}
+
+//To find in bw missing elements in array 
+#include <iostream>
+using namespace std;
+int missingInBwElement(int A[],int n){
+    int diff=A[0]-0;
+    for(int i=0;i<n;i++){
+        if(A[i]-i!=diff){
+            return i+diff;
+        }
+    }
+    return -1;
+}
+int main(){
+    int A[] = {6,7,8,9,10,11,13,14};
+     int n = sizeof(A)/sizeof(A[0]);
+    cout<<"Missing element is: "<<missingInBwElement(A,n);
     return 0;
 }
