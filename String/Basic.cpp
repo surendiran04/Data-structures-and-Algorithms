f88e6e5ba8eeada57facfd8b7de9cae411c6ba51
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-17 17:05:22 +0530

    Update Basic.cpp

--- String/Basic.cpp
+++ String/Basic.cpp
@@ -9,7 +9,16 @@ int main() {
     for(i=0;s[i]!='\0';i++){
         
     }
-    printf("Length is %d",i); //includes the '\0' 
+    printf("Length is %d\n",i); //includes the '\0'  
+    for(i=0;s[i]!='\0';i++){ //changing to lowercase if the input is Uppercase eg: A-65  65+32=97 (a-97)
+        if(s[i]>=65 && s[i]<=90){  //changing to lowercase
+            s[i]+=32;
+        }
+        else if(s[i]>=97 && s[i]<=122){ //changing to uppercase
+            s[i]-=32;
+        }    
+    }
+    printf("String: %s\n", s);
     char sentence[100]; 
     char source[] = "This is another sentence.";
     strcpy(sentence, source);  //included in #include <string.h>
