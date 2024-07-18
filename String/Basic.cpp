09a7427c6d4de4fafda477991f7e9fe9154bf872
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-18 21:45:16 +0530

    Update Basic.cpp

--- String/Basic.cpp
+++ String/Basic.cpp
@@ -124,3 +124,31 @@ int main() {
     }
     return 0;
 }
+
+#include <stdio.h> //permutation - order matters
+void swap(char* x, char *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+void strPerSwap(char *A, int l, int h){
+    int i;
+    if(l==h){
+        printf("%s\n", A);
+    }
+    else{
+        for(i=l;i<=h;i++){
+            swap(&A[l],&A[i]);
+            strPerSwap(A, l+1, h);
+            swap(&A[l],&A[i]);
+        }
+    }
+}
+int main(){
+    int i=0;
+    char A[] = "ABC";
+    for(; A[i]!=0; i++);
+    strPerSwap(A, 0, i-1);
+    return 0;
+
+};
