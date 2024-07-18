12bee472c8da951a76dc8671df6d0d481edaad18
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-18 12:48:06 +0530

    Update Basic.cpp

--- String/Basic.cpp
+++ String/Basic.cpp
@@ -98,3 +98,29 @@ int main(){  //comparing strings
     cout<<"Strings are equal";
     return 0;
 }
+
+#include <iostream> //Dupliacte in a string
+int main() {
+    char a[]="findingsssss";
+    long int h1=0; 
+    long int h2=0; 
+    long int x=0;
+    for (int i=0; a[i] != '\0'; i++) {
+        x = 1;
+        x = x << (a[i]-97);
+        if ( (x&h1) > 0 )
+            h2 = (x&h1) | h2; 
+        else
+            h1 = x | h1;
+    }
+    x = 1;
+    int count{};
+    while (count >= 0 && count <= 25) {
+        if ( (h2 & x) > 0) {
+            std::cout << char(count+97) << " is duplicate." << std::endl;
+        }
+        x = x << 1;
+        count++;
+    }
+    return 0;
+}
