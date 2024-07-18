2c54029b76e1fa3db0c81e9f613ec43db0241010
Surendiran <137646072+surendiran04@users.noreply.github.com>
2024-07-18 22:22:50 +0530

    Update Basic.cpp

--- String/Basic.cpp
+++ String/Basic.cpp
@@ -148,7 +148,29 @@ int main(){
     int i=0;
     char A[] = "ABC";
     for(; A[i]!=0; i++);
-    strPerSwap(A, 0, i-1);
+    strPerSwap(A, 0, i-1); //i-1 is length-1
     return 0;
 
+};
+
+class Solution { //c++ permutation
+private: 
+    void recurPermute(int index, vector<int> &nums, vector<vector<int>> &ans) {
+        if(index == nums.size()) {
+            ans.push_back(nums); 
+            return; 
+        }
+        for(int i = index;i<nums.size();i++) {
+            swap(nums[index], nums[i]); 
+            recurPermute(index+1, nums, ans); 
+            swap(nums[index], nums[i]); 
+        }
+        
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans; 
+        recurPermute(0, nums, ans); 
+        return ans; 
+    }
 };
